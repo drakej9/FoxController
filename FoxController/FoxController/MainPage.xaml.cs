574dbfb282405a60bce77cfbfdc77443746080df
J. Drake <drakej99@hotmail.com>
2016-03-10 21:02:12 -0500

    initial and method layout

---
+++ FoxController/FoxController/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FoxController
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private struct Tone
+        {
+            private string name;
+            private string path;
+
+            public Tone(string newName, string newPath)
+            {
+                name = newName;
+                path = newPath;
+            }
+
+            public string getName()
+            {
+                return this.name;
+            }
+
+            public string getPath()
+            {
+                return this.path;
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void initializeGpio()
+        {
+
+        }
+
+        private void respondToIdSwitch()
+        {
+
+        }
+
+        private void respondToStartStopSwitch()
+        {
+
+        }
+
+        private void playId()
+        {
+
+        }
+
+        private void playTones(int numTones)
+        {
+
+        }
+
+        private void playTone(Tone tone)
+        {
+
+        }
+        
+
+    }
+}
