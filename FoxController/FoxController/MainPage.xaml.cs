0223f66d9ba5bb91062a15b85d01895fe606101f
J. Drake <...>
2016-03-11 18:10:37 -0500

    added gpio info; began ID synthesis

--- FoxController/FoxController/MainPage.xaml.cs
+++ FoxController/FoxController/MainPage.xaml.cs
@@ -15,6 +15,9 @@ using Windows.UI.Xaml.Navigation;
 using Windows.Media.Core;
 using Windows.Media.Playback;
 using Windows.Storage;
+using Windows.Media.SpeechSynthesis;
+using Windows.Devices.Gpio;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,6 +29,9 @@ namespace FoxController
     public sealed partial class MainPage : Page
     {
         MediaPlayer player;
+        MediaElement mediaElement = new MediaElement();
+        private const int BUTTON_PIN = 4;
+        private GpioPin buttonPin;
         private struct Tone
         {
             private string name;
@@ -56,12 +62,33 @@ namespace FoxController
 
         private void initializeGpio()
         {
+            var gpio = GpioController.GetDefault();
+            // Show an error if there is no GPIO controller
+
+            //if (gpio == null)
+            //{
+            //    gpioStatus.Text = "There is no GPIO controller on this device.";
+            //    return;
+            //}
+
+            buttonPin = gpio.OpenPin(BUTTON_PIN);
+
+            // Check if input pull-up resistors are supported
+            if (buttonPin.IsDriveModeSupported(GpioPinDriveMode.InputPullUp))
+                buttonPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+            else
+                buttonPin.SetDriveMode(GpioPinDriveMode.Input);
+
+            // Set a debounce timeout to filter out switch bounce noise from a button press
+            buttonPin.DebounceTimeout = TimeSpan.FromMilliseconds(50);
+
+            buttonPin.ValueChanged += respondToIdSwitch;
 
         }
 
-        private void respondToIdSwitch()
+        private void respondToIdSwitch(GpioPin sender, GpioPinValueChangedEventArgs e)
         {
-
+            playId();
         }
 
         private void respondToStartStopSwitch()
@@ -69,9 +96,12 @@ namespace FoxController
 
         }
 
-        private void playId()
+        private async void playId()
         {
-
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("This is the C A L L S I G N fox transmitter.");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
         }
 
         private void playTones(int numTones)
