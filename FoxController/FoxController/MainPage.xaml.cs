42f3a0d834d38d09df63a8b4de5bbcb271d16453
J. Drake <...>
2016-04-04 19:10:54 -0400

    Removed ID button; removed all code related to tones; refactored

--- FoxController/FoxController/MainPage.xaml.cs
+++ FoxController/FoxController/MainPage.xaml.cs
@@ -1,23 +1,14 @@
 ﻿using System;
+//using System.IO;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Windows.Media.Core;
-using Windows.Media.Playback;
 using Windows.Storage;
 using Windows.Media.SpeechSynthesis;
 using Windows.Devices.Gpio;
 using Windows.UI.Core;
+using Windows.System.Threading;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,37 +20,21 @@ namespace FoxController
     public sealed partial class MainPage : Page
     {
         MediaElement mediaElement = new MediaElement();
-        private const int BUTTON_PIN = 4;
-        private GpioPin buttonPin;
+        //private const int ID_PIN = 4;
+        private const int START_SEQUENCE_PIN = 5;
+        private bool sequenceStarted = true;
+        private TimeSpan period = TimeSpan.FromSeconds(20);
+        private ThreadPoolTimer periodicTimer;
+        
         SpeechSynthesisStream idStream;
-        private string id = "This is the C A L L S I G N fox transmitter.";
-        private struct Tone
-        {
-            private string name;
-            private string path;
-
-            public Tone(string newName, string newPath)
-            {
-                name = newName;
-                path = newPath;
-            }
-
-            public string getName()
-            {
-                return this.name;
-            }
-
-            public string getPath()
-            {
-                return this.path;
-            }
-        }
+        private string id = "This is the C A L L S I G N fox transmitter; part of the Boy Scout Troop 1 11 radio direction finding exercise.";
 
         public MainPage()
         {
             this.InitializeComponent();
             this.initializeGpio();
-            this.initializeId();
+            this.initializeAudio();
+            this.initializeTimer();
         }
 
         private void initializeGpio()
@@ -72,58 +47,80 @@ namespace FoxController
                 return;
             }
 
-            buttonPin = gpio.OpenPin(BUTTON_PIN);
+            //GpioPin idPin = gpio.OpenPin(ID_PIN);
+            GpioPin startSequencePin = gpio.OpenPin(START_SEQUENCE_PIN);
 
-            // Check if input pull-up resistors are supported
-            if (buttonPin.IsDriveModeSupported(GpioPinDriveMode.InputPullUp))
-                buttonPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
-            else
-                buttonPin.SetDriveMode(GpioPinDriveMode.Input);
+            // check if input pull-up resisitors are supprted
+            //checkAndSetPullUp(idPin);
+            checkAndSetPullUp(startSequencePin);
 
             // Set a debounce timeout to filter out switch bounce noise from a button press
-            buttonPin.DebounceTimeout = TimeSpan.FromMilliseconds(50);
+            //idPin.DebounceTimeout = TimeSpan.FromMilliseconds(50);
+            startSequencePin.DebounceTimeout = TimeSpan.FromMilliseconds(50);
 
-            buttonPin.ValueChanged += respondToIdSwitch;
-
+            //idPin.ValueChanged += respondToIdSwitch;
+            startSequencePin.ValueChanged += respondToStartSequenceSwitch;
         }
 
-        private async void initializeId()
+        private void checkAndSetPullUp(GpioPin pin)
+        {
+            // Check if input pull-up resistors are supported
+            if (pin.IsDriveModeSupported(GpioPinDriveMode.InputPullUp))
+                pin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+            else
+                pin.SetDriveMode(GpioPinDriveMode.Input);
+        }
+
+        private async void initializeAudio()
         {
             var synth = new SpeechSynthesizer();
             idStream = await synth.SynthesizeTextToStreamAsync(id);
         }
 
-        private void respondToIdSwitch(GpioPin sender, GpioPinValueChangedEventArgs e)
+        private void initializeTimer()
         {
-            var task = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            periodicTimer = ThreadPoolTimer.CreatePeriodicTimer(async (CoreComponentInputSource) =>
             {
-                playId();
-            });
+                await Dispatcher.RunAsync(CoreDispatcherPriority.High,
+                () =>
+                {
+                    if (sequenceStarted == true)
+                    {
+                        //playId();
+                        mediaElement.SetSource(idStream, idStream.ContentType);
+                        mediaElement.Position = new TimeSpan(0, 0, 0);
+                        mediaElement.Play();
+                    }
+                });
+            }, period);
         }
 
-        private void respondToStartStopSwitch()
-        {
+        //private void respondToIdSwitch(GpioPin sender, GpioPinValueChangedEventArgs e)
+        //{
+            
+        //    var task = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+        //    {
+        //        playId();
+        //    });
+        //}
 
+        private void respondToStartSequenceSwitch(GpioPin sender, GpioPinValueChangedEventArgs e)
+        {
+            if (sequenceStarted == false)
+            {
+                sequenceStarted = true;
+            }
+            else
+            {
+                sequenceStarted = false;
+            }
         }
 
         private void playId()
         {
             mediaElement.SetSource(idStream, idStream.ContentType);
+            mediaElement.Position = new TimeSpan(0,0,0);
             mediaElement.Play();
         }
-
-        private void playTones(int numTones)
-        {
-
-        }
-
-        //private async void playTone(Tone tone)
-        //{
-        //    StorageFile file = await StorageFile.GetFileFromPathAsync(tone.getPath());
-        //    mediaElement.SetFileSource(file);
-        //    mediaElement.Play();
-        //}
-        
-
     }
 }
